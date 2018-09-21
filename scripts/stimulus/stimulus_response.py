e9fac7d0723317a57301bf2a09c8adb7ab942e73
Jeff Zaremba <jzaremba@gmail.com>
2018-09-21 15:41:08 -0400

    Moved single trial traces out to a separate script

--- scripts/stimulus/stimulus_response.py
+++ scripts/stimulus/stimulus_response.py
@@ -1,40 +1,16 @@
 """Look at stimulus responses."""
-import matplotlib.pyplot as plt
+import matplotlib as mpl
 import os
-from sys import argv
+if 'DISPLAY' not in os.environ:
+    mpl.use('Agg')
 
-from pool.plotting import stimulus as pps
-from pool.layouts import stimulus as pls
+import os
 
 import flow
 from flow import misc
-from flow.metadata2 import DateSorter
+import flow.metadata2 as metadata
 
-
-def parse_args_orig():
-    """Parse arguments."""
-    defaults = {
-        'graph': 'stimulus',
-        'sort': '',  # Analysis upon which to sort
-        'analyses': [],  # Analyses to display along right side
-        'color-max': 'auto',
-        'trace-type': 'dff',
-        'trange-ms': (-1000, 8000),
-        'baseline-ms': (-1000, 0),
-        'display': 'dff',
-        'remove-licking': False,
-        'error-trials': -1,  # -1 is off, 0 is correct trials, 1 is error trials, 2 is diff of error trials
-        'hungry-sated': 0,  # 0 is hungry trials, 1 is sated trials, 2 is hungry-sated
-    }
-
-    lpars = parseargv.extractkv(argv, defaults)
-    if not isinstance(lpars['analyses'], list):
-        lpars['analyses'] = [lpars['analyses']]
-
-    days = parseargv.sorteddays(argv, classifier=False, trace=False, force=True)
-
-    print lpars, days
-    from pudb import set_trace; set_trace()
+from pool.plotting import stimulus as pps
 
 
 def parse_args():
@@ -65,13 +41,6 @@ def parse_args():
     return args
 
 
-def save_pdf(figs, save_path):
-    """Save figs to a pdf."""
-    # header_fig = misc.summary_page(days, lpars)
-    # figs.insert(0, header_fig)
-    misc.save_figs(save_path, figs)
-
-
 def generate_fig(sorter, trace_type, t_range_s, errortrials, baseline):
     """Do the real plotting."""
     fig, axs = flow.misc.plotting.layout_subplots(
@@ -84,38 +53,21 @@ def generate_fig(sorter, trace_type, t_range_s, errortrials, baseline):
     return fig
 
 
-def traces_per_cell(sorter, trace_type, t_range_s, errortrials, baseline):
-    for date in sorter:
-        for roi_idx in [0,1,2]:
-            fig = plt.figure()
-            pls.all_stimulus_traces(
-                fig, date, roi_idx, start_s=t_range_s[0], end_s=t_range_s[1],
-                trace_type=trace_type, errortrials=errortrials,
-                baseline=baseline)
-            from pudb import set_trace; set_trace()
-
-
-
 def main():
     """Main function."""
     filename = 'stimulus_response_{}.pdf'.format(misc.datestamp())
     save_path = os.path.join(
         flow.paths.graphd, 'stimulus_response', filename)
     args = parse_args()
-    sorter = DateSorter.frommeta(
+    sorter = metadata.DateSorter.frommeta(
         mice=args.mice, dates=args.dates, tags=args.tags)
-
-    traces_per_cell(sorter, args.trace_type, args.t_range_s, args.errortrials,
-                    args.baseline)
-
-
     fig = generate_fig(
-        sorter, args.trace_type, args.t_range_s, args.error_trials,
+        sorter, args.trace_type, args.t_range_s, args.errortrials,
         args.baseline)
-    save_pdf([fig], save_path)
+    summary_fig = misc.summary_page(sorter, figsize=(16, 9), **vars(args))
+    misc.save_figs(save_path, [summary_fig, fig])
     print(save_path)
 
 
 if __name__ == '__main__':
     main()
-    # parse_args()
