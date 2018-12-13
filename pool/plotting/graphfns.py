25f667589d115ec73754e01dc2fff6c4e1b09a0d
Jeff Zaremba <jzaremba@gmail.com>
2018-12-13 09:24:49 -0500

    Fixes for building docs

--- pool/plotting/graphfns.py
+++ pool/plotting/graphfns.py
@@ -252,19 +252,19 @@ def reducepoints(x, y, n=2000, further=True):
 
 # Define a context manager to suppress stdout and stderr.
 class SuppressErrors(object):
-    """
-    A context manager for doing a "deep suppression" of stdout and stderr in
-    Python, i.e. will suppress all print, even if the print originates in a
+    """A context manager for doing a "deep suppression" of stdout and stderr.
+
+    Will suppress all print, even if the print originates in a
     compiled C/Fortran sub-function.
-       This will not suppress raised exceptions, since exceptions are printed
+
+    This will not suppress raised exceptions, since exceptions are printed
     to stderr just before a script exits, and after the context manager has
     exited (at least, I think that is why it lets exceptions through).
 
     """
-
     def __init__(self):
         # Open a pair of null files
-        self.null_fds =  [os.open(os.devnull,os.O_RDWR) for x in range(2)]
+        self.null_fds = [os.open(os.devnull, os.O_RDWR) for x in range(2)]
         # Save the actual stdout (1) and stderr (2) file descriptors.
         self.save_fds = (os.dup(1), os.dup(2))
 
