431965ee09949654c623d1a9f2f3da0ad29cf620
Jeff Zaremba <jzaremba@gmail.com>
2018-12-28 21:50:34 -0500

    Reworked new versions of layouts. Moved a lot of functionality out of pool/plotting/reactivation.py

--- pool/layouts/reactivation.py
+++ pool/layouts/reactivation.py
@@ -1,15 +1,18 @@
 """Reactivation figure layouts."""
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+from pandas import IndexSlice as Idx
 import seaborn as sns
 
 from flow.misc.plotting import right_label
 
 from .. import config
 from ..plotting import reactivation as react
+from .. import dataframes as dfs
 
 
-def probability_throughout_trials(runs, pre_s=2, post_s=None):
+def classifier_throughout_trials(runs, pre_s=-2, post_s=None, limit_conditions=False):
     """Layout reactivation probability trial plots.
 
     Lays out an array of 2*n_trial_types x n_replay_types array of plots
@@ -28,7 +31,12 @@ def probability_throughout_trials(runs, pre_s=2, post_s=None):
     fig : matplotlib.pyplot.Figure
 
     """
-    trial_types = ['plus', 'neutral', 'minus', 'pavlovian', 'blank']
+    df = dfs.reactivation.trial_classifier_df(runs)
+
+    if limit_conditions:
+        trial_types = ['plus', 'neutral', 'minus']
+    else:
+        trial_types = ['plus', 'neutral', 'minus', 'pavlovian', 'blank']
     replay_types = config.stimuli()
 
     fig, axs = plt.subplots(
@@ -38,20 +46,20 @@ def probability_throughout_trials(runs, pre_s=2, post_s=None):
     for axs_row, trial_type in zip(axs[::2], trial_types):
         for ax, replay_type in zip(axs_row, replay_types):
             react.reactivation_probability_throughout_trials(
-                ax, runs, trial_type=trial_type, replay_type=replay_type,
+                ax, df, trial_type=trial_type, replay_type=replay_type,
                 pre_s=pre_s, post_s=post_s, errortrials=0, label='correct')
 
     for axs_row, trial_type in zip(axs[1::2], trial_types):
         for ax, replay_type in zip(axs_row, replay_types):
             react.reactivation_probability_throughout_trials(
-                ax, runs, trial_type=trial_type, replay_type=replay_type,
+                ax, df, trial_type=trial_type, replay_type=replay_type,
                 pre_s=pre_s, post_s=post_s, errortrials=1, label='error',
                 linestyle='--')
 
     for ax, replay_type in zip(axs[0, :], replay_types):
-        ax.set_title(replay_type)
+        ax.set_title('{} replays'.format(replay_type))
     for ax, trial_type in zip(axs[::2, -1], trial_types):
-        right_label(ax, trial_type)
+        right_label(ax, '{}\ntrials'.format(trial_type))
     for ax in axs[::2, 0]:
         ax.set_ylabel('correct\nreplay probability')
     for ax in axs[1::2, 0]:
@@ -62,9 +70,40 @@ def probability_throughout_trials(runs, pre_s=2, post_s=None):
     return fig
 
 
-def event_distributions_throughout_trials(
-        df, pre_s=5, post_s=10, exclude_window=(-0.1, 2.5),
-        threshold=0.1, limit_conditions=False):
+def event_distribution_throughout_trials(
+        runs, pre_s=-5, post_s=10, exclude_window=(-0.1, 2.5), threshold=0.1,
+        kind='kde', limit_conditions=False, inactivity_mask=False,
+        **plot_kwargs):
+    """
+    Plot continuous event distribution throughout trial.
+
+    Parameters
+    ----------
+    runs : RunSorter or list of Run
+    pre_s, post_s : float
+        Start and stop trial times to plot, relative to stim onset.
+    exclude_window : 2-element tuple
+        Start and stop times to exclude from plotting. Intended to mask out
+        stimulus time.
+    threshold : float
+        Classifier reactivation confidence threshold.
+    kind : str
+        Type of plot to plot.
+    limit_conditions : bool
+        If True, only look at plus, neutral, and minus conditions.
+    inactvitiy_mask : bool
+        If True, limit reactivations to periods of inactivity.
+    **plot_kwargs
+        Additional arguments are passed directly to the plotting functions.
+
+    Returns
+    -------
+    seaborn.GridSpec
+
+    """
+    df = dfs.reactivation.trial_events_df(
+        runs, threshold=threshold, xmask=False,
+        inactivity_mask=inactivity_mask)
 
     # Make sure Index levels are in the correct order
     df = df.reorder_levels(
@@ -73,15 +112,15 @@ def event_distributions_throughout_trials(
 
     # Limit to only {'plus', 'neutral', 'minus'}
     if limit_conditions:
-        df = df.loc(axis=0)[:, :, :, :, ['plus', 'neutral', 'minus'], :, :, :]
-        row_order=['plus', 'neutral', 'minus']
+        df = df.loc[Idx[:, :, :, :, ['plus', 'neutral', 'minus'], :, :, :], :]
+        row_order = ['plus', 'neutral', 'minus']
     else:
-        row_order=['plus', 'neutral', 'minus', 'pavlovian', 'blank']
+        row_order = ['plus', 'neutral', 'minus', 'pavlovian', 'blank']
 
     df = df.reset_index(['event_type', 'error', 'condition'])
 
     df = df[
-        (df.time > -pre_s) &
+        (df.time > pre_s) &
         (df.time < post_s) &
         ((df.condition == 'blank') |
             ((df.time < exclude_window[0]) |
@@ -93,15 +132,18 @@ def event_distributions_throughout_trials(
         margin_titles=True)
 
     g.map(plt.axvline, x=0, ls=':', color='k')
-    g.map(sns.distplot, 'time', rug=True, hist=False, kde=True)
+    if kind == 'kde':
+        g.map(sns.distplot, 'time', rug=True, hist=False, kde=True,
+              **plot_kwargs)
+    else:
+        raise ValueError("Unrecognized 'kind' argument.")
     g.add_legend()
     if exclude_window is not None:
         g.map(
             plt.fill_betweenx, y=plt.ylim(), x1=exclude_window[0],
             x2=exclude_window[1], color='0.9')
 
-    # g.set(xlim=(-pre_s, post_s), ylim=(0, 0.2))
-    g.set(xlim=(-pre_s, post_s))
+    g.set(xlim=(pre_s, post_s))
     g.set_xlabels('Time from stim onset (s)')
     g.set_ylabels('Normalized density')
 
@@ -109,33 +151,212 @@ def event_distributions_throughout_trials(
 
 
 def binned_event_distrbituions_throughout_trials(
-        runs, pre_s=5, iti_start_s=5, iti_end_s=10, stim_pad_s=0.1,
-        threshold=0.1, kind='bar', limit_conditions=False, **plot_kwargs):
-    edges = [-pre_s, -stim_pad_s, 0, 2, 2 + stim_pad_s, iti_start_s, iti_end_s]
-    bin_labels = ['pre', 'pre_buffer', 'stim', 'post_buffer', 'post', 'iti']
+        runs, pre_s=-5, iti_start_s=5, iti_end_s=10, threshold=0.1, kind='bar',
+        limit_conditions=False, inactivity_mask=False, **plot_kwargs):
+    """
+    Plot reactivation rates in different intervals of the trial.
 
-    all_events = []
-    for run in runs:
-        events, times = react._events_by_trial(run, threshold, xmask=False)
+    By default plots pre-stimulus baseline, post-stimulus response window, and
+    ITI period.
 
-        events_binned = react._bin_events(events, times, edges, bin_labels)
-        all_events.append(events_binned)
-    events_binned = pd.concat(all_events, axis=0)
+    Parameters
+    ----------
+    runs : RunSorter or list of Run
+    pre_s : float
+        Start of first interval, relative to stim onset.
+    iti_start_s, iti_end_s : float
+        Start and stop times (relative to stim onset) for the ITI period.
+    threshold : float
+        Classifier reactivation confidence threshold.
+    kind : str
+        Type of plot to plot.
+    limit_conditions : bool
+        If True, only look at plus, neutral, and minus conditions.
+    inactvitiy_mask : bool
+        If True, limit reactivations to periods of inactivity.
+    **plot_kwargs
+        Additional arguments are passed directly to the plotting functions.
 
-    events_binned = events_binned[
-        events_binned.time_cat.isin(['pre', 'post', 'iti'])]
+    Returns
+    -------
+    seaborn.GridSpec
+
+    """
+    # Update some plot_kwargs
+    if kind == 'bar' and 'ci' not in plot_kwargs:
+        plot_kwargs['ci'] = 68  # Plot SEM as error bars
+
+    # These should probably match pool.analyses.trialdf event paddings
+    pre_stim_pad = -0.1
+    post_stim_pad = 2.5
+    edges = [pre_s, pre_stim_pad, 0, 2, post_stim_pad, iti_start_s, iti_end_s]
+    bin_labels = ['pre', 'pre-buffer', 'stim', 'post-buffer', 'post', 'iti']
+
+    events = dfs.reactivation.trial_events_df(
+        runs, threshold=threshold, xmask=False,
+        inactivity_mask=inactivity_mask)
+    frames = dfs.imaging.trial_frames_df(
+        runs, inactivity_mask=inactivity_mask)
+
+    row_order = ['plus', 'neutral', 'minus', 'pavlovian', 'blank']
+    if limit_conditions:
+        row_order = ['plus', 'neutral', 'minus']
+        events = (events
+                  .reorder_levels(['mouse', 'date', 'run', 'trial_idx',
+                                   'condition', 'error', 'event_type',
+                                   'event_idx'])
+                  .loc[Idx[:, :, :, :, ['plus', 'neutral', 'minus']], :]
+                  )
+
+        frames = (frames
+                  .reorder_levels(['mouse', 'date', 'run', 'trial_idx',
+                                   'condition', 'error'])
+                  .loc[Idx[:, :, :, :, ['plus', 'neutral', 'minus']], :]
+                  )
+
+    events_binned = (dfs.bin_events(events, frames, edges, bin_labels)
+                     .reorder_levels(['mouse', 'date', 'run', 'event_type',
+                                      'condition', 'error', 'time_cat'])
+                     .loc[Idx[:, :, :, :, :, :, ['pre', 'post', 'iti']], :]
+                     .reset_index(['event_type', 'condition', 'error',
+                                   'time_cat'])
+                     )
     events_binned.time_cat.cat.remove_unused_categories(inplace=True)
 
     g = sns.catplot(
         x='time_cat', y='event_rate', col='event_type', row='condition',
         hue='error', data=events_binned, kind=kind, margin_titles=True,
-        row_order=['plus', 'neutral', 'minus', 'pavlovian', 'blank'],
-        col_order=['plus', 'neutral', 'minus'], **plot_kwargs)
+        row_order=row_order, col_order=['plus', 'neutral', 'minus'],
+        **plot_kwargs)
 
     g.set_xlabels('')
     g.set_ylabels('Event rate (Hz)')
 
     return g
+
+
+def histogram_event_distrbituions_throughout_trials(
+        runs, pre_s=-5, post_s=10, bin_size_s=1, exclude_window=(-0.1, 2.5), threshold=0.1, kind='bar',
+        limit_conditions=False, inactivity_mask=False, **plot_kwargs):
+    """
+    Plot reactivation rates like a histogram (1-s bins) throughout trial.
+
+    Parameters
+    ----------
+    runs : RunSorter or list of Run
+    pre_s, post_s : float
+        Start and stop trial times to plot, relative to stim onset.
+    bin_size_s : float
+        Bin size, in seconds.
+    exclude_window : 2-element tuple
+        Start and stop times to exclude from plotting. Intended to mask out
+        stimulus time.
+    threshold : float
+        Classifier reactivation confidence threshold.
+    kind : str
+        Type of plot to plot.
+    limit_conditions : bool
+        If True, only look at plus, neutral, and minus conditions.
+    inactvitiy_mask : bool
+        If True, limit reactivations to periods of inactivity.
+    **plot_kwargs
+        Additional arguments are passed directly to the plotting functions.
+
+    Returns
+    -------
+    seaborn.GridSpec
+
+    """
+    # Update some plot_kwargs
+    if kind == 'bar' and 'ci' not in plot_kwargs:
+        plot_kwargs['ci'] = 68  # Plot SEM as error bars
+
+    left_edges = np.arange(pre_s, post_s, bin_size_s)
+    edges = np.concatenate([left_edges, [left_edges[-1] + bin_size_s]])
+    bin_labels = [str(x) for x in left_edges]
+
+    events = dfs.reactivation.trial_events_df(
+        runs, threshold=threshold, xmask=False,
+        inactivity_mask=inactivity_mask)
+    frames = dfs.imaging.trial_frames_df(
+        runs, inactivity_mask=inactivity_mask)
+
+    if exclude_window is not None:
+        events = events.reset_index(['condition'])
+        events = events[(events.condition == 'blank') |
+                        ((events.time < exclude_window[0]) |
+                        (events.time > exclude_window[1]))]
+        events = (events
+                  .set_index(['condition'], append=True)
+                  .reorder_levels(['mouse', 'date', 'run', 'trial_idx',
+                                   'condition', 'error', 'event_type',
+                                   'event_idx'])
+                  )
+
+    row_order = ['plus', 'neutral', 'minus', 'pavlovian', 'blank']
+    if limit_conditions:
+        row_order = ['plus', 'neutral', 'minus']
+        events = (events
+                  .reorder_levels(['mouse', 'date', 'run', 'trial_idx',
+                                   'condition', 'error', 'event_type',
+                                   'event_idx'])
+                  .loc[Idx[:, :, :, :, ['plus', 'neutral', 'minus']], :]
+                  )
+
+        frames = (frames
+                  .reorder_levels(['mouse', 'date', 'run', 'trial_idx',
+                                   'condition', 'error'])
+                  .loc[Idx[:, :, :, :, ['plus', 'neutral', 'minus']], :]
+                  )
+
+    events_binned = (dfs.bin_events(events, frames, edges, bin_labels)
+                     .reset_index(['event_type', 'condition', 'error',
+                                   'time_cat'])
+                     )
+
+    # Not really sure why this is sometimes a categorical series and sometimes
+    # not.
+    # events_binned.time_cat.cat.remove_unused_categories(inplace=True)
+
+    g = sns.catplot(
+        x='time_cat', y='event_rate', col='event_type', row='condition',
+        hue='error', data=events_binned, kind=kind, margin_titles=True,
+        row_order=row_order, col_order=['plus', 'neutral', 'minus'],
+        **plot_kwargs)
+
+    g.set_xlabels('')
+    g.set_ylabels('Event rate (Hz)')
+
+    return g
+
+# def binned_event_distrbituions_throughout_trials_orig(
+#         runs, pre_s=-5, iti_start_s=5, iti_end_s=10, stim_pad_s=0.1,
+#         threshold=0.1, kind='bar', limit_conditions=False, **plot_kwargs):
+#     edges = [pre_s, -stim_pad_s, 0, 2, 2 + stim_pad_s, iti_start_s, iti_end_s]
+#     bin_labels = ['pre', 'pre_buffer', 'stim', 'post_buffer', 'post', 'iti']
+
+#     all_events = []
+#     for run in runs:
+#         events, times = react._events_by_trial(run, threshold, xmask=False)
+
+#         events_binned = react._bin_events(events, times, edges, bin_labels)
+#         all_events.append(events_binned)
+#     events_binned = pd.concat(all_events, axis=0)
+
+#     events_binned = events_binned[
+#         events_binned.time_cat.isin(['pre', 'post', 'iti'])]
+#     events_binned.time_cat.cat.remove_unused_categories(inplace=True)
+
+#     g = sns.catplot(
+#         x='time_cat', y='event_rate', col='event_type', row='condition',
+#         hue='error', data=events_binned, kind=kind, margin_titles=True,
+#         row_order=['plus', 'neutral', 'minus', 'pavlovian', 'blank'],
+#         col_order=['plus', 'neutral', 'minus'], **plot_kwargs)
+
+#     g.set_xlabels('')
+#     g.set_ylabels('Event rate (Hz)')
+
+#     return g
 
 
 def peri_event_behavior(df, limit_conditions=False, **plot_kwargs):
@@ -160,7 +381,7 @@ def peri_event_behavior(df, limit_conditions=False, **plot_kwargs):
             grouped['condition'].isin(['plus', 'neutral', 'minus'])]
         row_order = ['plus', 'neutral', 'minus']
     else:
-        row_order=['plus', 'neutral', 'minus', 'pavlovian', 'blank']
+        row_order = ['plus', 'neutral', 'minus', 'pavlovian', 'blank']
 
     g = sns.catplot(
         x='trial_idx', y='error', col='event_type', row='condition',
--- pool/plotting/reactivation.py
+++ pool/plotting/reactivation.py
@@ -1,13 +1,12 @@
 """Reactivation plotting functions."""
-import numpy as np
-import pandas as pd
+from pandas import IndexSlice as Idx
 
 from .. import config
 
 
 def reactivation_probability_throughout_trials(
-        ax, runs, trial_type='plus', replay_type='plus',
-        pre_s=2, post_s=None, errortrials=-1, **kwargs):
+        ax, df, trial_type='plus', replay_type='plus',
+        pre_s=-2, post_s=None, errortrials=-1, **kwargs):
     """Plot the classifier probability throughout the trial.
 
     Parameters
@@ -18,10 +17,9 @@ def reactivation_probability_throughout_trials(
         Type of trial to plot data from.
     replay_type : str
         Type of replay to plot classifier probabilities for.
-    pre_s : float
-        Time in seconds prior to stim onset.
-    post_s : float, optional
-        If not None, only include this duration after stimulus (in seconds).
+    pre_s, post_s : float, optional
+        Time in seconds relative to stim onset to include. Negative value for
+        before stim.
     errortrials : {-1, 0, 1}
         -1 is all trials, 0 is correct trials, 1 is error trials
 
@@ -32,265 +30,24 @@ def reactivation_probability_throughout_trials(
     if 'label' not in kwargs:
         kwargs['label'] = replay_type
 
-    all_results = [pd.DataFrame()]
-    for run in runs:
-        all_results.append(_classifier_by_trial(
-            run, pre_s=pre_s, post_s=post_s,
-            errortrials=errortrials))
-    result = pd.concat(all_results, axis=0)
+    if errortrials == -1:
+        error_slice = slice(None)
+    elif errortrials == 0:
+        error_slice = False
+    elif errortrials == 1:
+        error_slice = True
 
-    grouped = result.groupby(level=['condition', 'time']).mean()
+    # Always make sure levels are in expected order before slicing
+    df = df.reorder_levels(['mouse', 'date', 'run', 'trial_idx',
+                            'condition', 'error', 'time'])
+    trimmed_df = df.loc[
+        Idx[:, :, :, :, trial_type, error_slice, :],  # Row slice
+        replay_type]                                  # Column slice
 
-    # We could have no trials of the desired error type (or trial type in general)
-    if trial_type in grouped.index:
-        ax.plot(grouped.xs(trial_type).loc[:, replay_type], **kwargs)
+    grouped = trimmed_df.groupby(level=['time']).mean()
 
-    # def percentile10(x):
-    #     return np.percentile(x, 0.1)
-    # def percentile90(x):
-    #     return np.percentile(x, 0.9)
-    # grouped = result.groupby(level=['condition', 'time']).agg([np.median, percentile10, percentile90])
-    # ax.fill_between(grouped.loc[trial_type, replay_type].index,
-    #                 grouped.loc[trial_type, (replay_type, 'percentile10')],
-    #                 grouped.loc[trial_type, (replay_type, 'percentile90')], alpha=0.5, **kwargs)
-
-
-# def reactivation_events_throughout_trials(
-#         ax, runs, pre_s=5, iti_start_s=5, iti_end_s=10, stim_pad_s=0.1,
-#         threshold=0.1):
-#     edges = [-pre_s, -stim_pad_s, 0, 2, 2 + stim_pad_s, iti_start_s, iti_end_s]
-#     bin_labels = ['pre', 'pre_buffer', 'stim', 'post_buffer', 'post', 'iti']
-
-#     for run in runs:
-#         events, times = _events_by_trial(run, threshold, xmask=False)
-
-#         events_binned = _bin_events(events, times, edges, bin_labels)
-
-
-def _classifier_by_trial(run, pre_s=2, post_s=None, errortrials=-1):
-    """Return the classifier probability of all stimuli around stim presentations.
-
-    Parameters
-    ----------
-    c2p : flow.classify2p.Classify2p
-    t2p : flow.trace2p.Trace2p
-    pre_s : float
-        Time in seconds prior to stim onset.
-    post_s : float, optional
-        If not None, only include this duration after stimulus (in seconds).
-    errortrials : {-1, 0, 1}
-        -1 is all trials, 0 is correct trials, 1 is error trials
-
-    """
-    c2p = run.classify2p()
-    t2p = run.trace2p()
-
-    classifier_results = c2p.results()
-    all_onsets = t2p.csonsets()
-    conditions = t2p.conditions()
-    errors = t2p.errors(cs=None)
-    replay_types = config.stimuli()
-
-    # Figure out last offset. Either last frame or largest stim onset diff,
-    # whichever is smaller
-    max_onset_diff = np.diff(all_onsets).max()
-    last_offset = t2p.nframes if t2p.nframes - all_onsets[-1] <= max_onset_diff \
-        else all_onsets[-1] + max_onset_diff
-
-    next_onsets = np.concatenate([all_onsets[1:], last_offset], axis=None)
-
-    fr = t2p.framerate
-    pre_f = int(np.ceil(pre_s * fr))
-
-    result = [pd.DataFrame()]
-    for trial_idx, (onset, next_onset, cond, err) in enumerate(
-            zip(all_onsets, next_onsets, conditions, errors)):
-        if errortrials == 0 and err:
-            continue
-        elif errortrials == 1 and not err:
-            continue
-
-        start_fr = onset - pre_f
-        if start_fr < 0:
-            trial_pre_s = pre_s - (-start_fr * fr)
-            start_fr = 0
-        else:
-            trial_pre_s = pre_s
-
-        trial_result = []
-        for replay_type in replay_types:
-            trial_replay_result = classifier_results[replay_type][
-                start_fr:next_onset - 1]
-            time = np.arange(len(trial_replay_result)) / fr - trial_pre_s
-
-            index = pd.MultiIndex.from_product(
-                [[trial_idx], [cond], time],
-                names=['trial_idx', 'condition', 'time'])
-            trial_result.append(
-                pd.Series(trial_replay_result, index=index, name=replay_type))
-        result.append(pd.concat(trial_result, axis=1))
-
-    final_result = pd.concat(result, axis=0)
-
-    if len(final_result) and post_s is not None:
-        final_result = final_result.loc(axis=0)[:, :, :post_s]
-
-    return final_result
-
-
-def _events_df(run, threshold=0.1, xmask=False, use_inactivity_mask=False):
-    import pool
-    t2p = run.trace2p()
-    c2p = run.classify2p()
-    events_list = [pd.DataFrame()]
-    if use_inactivity_mask:
-        mask = t2p.inactivity()
-    else:
-        mask = None
-    for event_type in pool.config.stimuli():
-        events = c2p.events(
-            event_type, threshold=threshold, traces=t2p, xmask=xmask,
-            mask=mask)
-        index = pd.MultiIndex.from_product(
-            [[run.mouse], [run.date], [run.run], [event_type],
-             np.arange(len(events))],
-            names=['mouse', 'date', 'run', 'event_type', 'event_idx'])
-        events_list.append(pd.DataFrame({'frame': events}, index=index))
-
-    return pd.concat(events_list, axis=0)
-
-
-def _frames_df(run, use_inactivity_mask=False):
-    t2p = run.trace2p()
-    frame_period = 1. / t2p.framerate
-    frames = np.arange(t2p.nframes)
-    if use_inactivity_mask:
-        frames = frames[t2p.inactivity()]
-    index = pd.MultiIndex.from_product(
-        [[run.mouse], [run.date], [run.run] * len(frames)],
-        names=['mouse', 'date', 'run'])
-    frames_df = pd.DataFrame({'frame': frames, 'frame_period': frame_period},
-                             index=index)
-    return frames_df
-
-
-def _events_by_trial(
-        run, threshold, xmask=False, next_onset_pad_s=0.1,
-        prev_onset_pad_s=2.5):
-    t2p = run.trace2p()
-
-    all_onsets = t2p.csonsets()
-    conditions = t2p.conditions()
-    errors = t2p.errors(cs=None)
-
-    next_onsets = np.concatenate([all_onsets[1:], t2p.nframes], axis=None)
-    prev_onsets = np.concatenate([0, all_onsets[:-1]], axis=None)
-
-    fr = t2p.framerate
-    next_onset_pad_fr = int(np.ceil(next_onset_pad_s * fr))
-    prev_onset_pad_fr = int(np.ceil(prev_onset_pad_s * fr))
-
-    events = _events_df(run, threshold, xmask=xmask)
-    frames = _frames_df(run)
-
-    result, times = [pd.DataFrame()], [pd.DataFrame()]
-    for trial_idx, (onset, next_onset, prev_onset, cond, err) in enumerate(zip(
-            all_onsets, next_onsets, prev_onsets, conditions, errors)):
-
-        trial_events = events.loc[
-            (events.frame >= prev_onset + prev_onset_pad_fr) &
-            (events.frame < next_onset - next_onset_pad_fr)].copy()
-        trial_events -= onset
-        trial_events['time'] = trial_events.frame / fr
-
-        trial_frames = frames.loc[
-            (frames.frame >= prev_onset + prev_onset_pad_fr) &
-            (frames.frame < next_onset - next_onset_pad_fr)].copy()
-        trial_frames.frame -= onset
-        trial_frames['time'] = trial_frames.frame * trial_frames.frame_period
-
-        # add in trial_idx, condition, error
-        trial_events = pd.concat(
-            [trial_events], keys=[trial_idx], names=['trial_idx'])
-        trial_events = pd.concat(
-            [trial_events], keys=[cond], names=['condition'])
-        trial_events = pd.concat(
-            [trial_events], keys=[err], names=['error'])
-
-        trial_frames = pd.concat(
-            [trial_frames], keys=[trial_idx], names=['trial_idx'])
-
-        result.append(trial_events)
-        times.append(trial_frames)
-    events_df = pd.concat(result)
-    events_df = events_df.reorder_levels(
-        ['mouse', 'date', 'run', 'trial_idx', 'condition', 'error',
-         'event_type', 'event_idx'])
-    events_df.drop(columns=['frame'], inplace=True)
-
-    times_df = pd.concat(times)
-    times_df = times_df.reorder_levels(
-        ['mouse', 'date', 'run', 'trial_idx'])
-    times_df.drop(columns=['frame'], inplace=True)
-
-    return events_df, times_df
-
-
-def _bin_events(events, times, edges, bin_labels):
-    events['time_cat'] = pd.cut(
-        events.time, edges, labels=bin_labels)
-    times['time_cat'] = pd.cut(
-        times.time, edges, labels=bin_labels)
-
-    events_gb = (events
-                 .groupby(
-                     ['mouse', 'date', 'run', 'trial_idx', 'condition',
-                      'error', 'event_type', 'time_cat'])
-                 .count()
-                 .dropna()
-                 .rename(columns={'time': 'events'}))
-
-    times_gb = (times
-                .groupby(
-                    ['mouse', 'date', 'run', 'trial_idx', 'time_cat',
-                     'frame_period'])
-                .count()
-                .dropna()
-                .reset_index('frame_period')
-                .rename(columns={'time': 'frames'}))
-
-    # There has to be a better way to do this (merge?)
-    # Expand across event types so that the merge will add in empty values
-    all_times = []
-    for event_type in config.stimuli():
-        all_times.append(times_gb
-                         .assign(event_type=event_type)
-                         .set_index('event_type', append=True))
-    times_gb = pd.concat(all_times)
-
-    result = pd.merge(
-        events_gb.reset_index(['condition', 'error']),
-        times_gb,
-        how='right',
-        on=['mouse', 'date', 'run', 'trial_idx', 'time_cat', 'event_type'])
-    result = result.reset_index(['time_cat', 'event_type'])
-
-    # Add in 0's
-    def fill_values(df):
-        df['condition'] = \
-            df['condition'].fillna(method='ffill').fillna(method='bfill')
-        df['error'] = df['error'].fillna(method='ffill').fillna(method='bfill')
-        df['events'] = df['events'].fillna(0)
-        return df
-
-    result = (result
-              .groupby(['mouse', 'date', 'run', 'trial_idx'])
-              .apply(fill_values))
-
-    # Reset error to a boolean
-    result['error'] = result['error'].astype('bool')
-
-    result['event_rate'] = \
-        result.events / (result.frame_period * result.frames)
-
-    return result
+    # # We could have no trials of the desired error type (or trial type in general)
+    # if len(grouped):
+    # Uses the slice object to correctly handle 'None' as an argument
+    to_plot = grouped.loc[slice(pre_s, post_s)]
+    ax.plot(to_plot, **kwargs)
