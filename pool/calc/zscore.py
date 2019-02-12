caefe358cebadca57d3dbac84c4deb092fb9b239
Kelly McGuire <kellyleecemcguire@gmail.com>
2019-02-12 16:27:34 -0500

    added additional mu and sigma types and switched away from using t2p dot d to dot trace

--- pool/calc/zscore.py
+++ pool/calc/zscore.py
@@ -9,7 +9,7 @@ from ..database import memoize
 
 
 @memoize(across='date', updated=190130, returns='cell array')
-def iti_mu(date, window=4):
+def iti_mu(date, window=4, nan_artifacts=False, thresh=8):
     """
     Calculate the mean (mu) of daily trace per cell avoiding visual
     stimulus period.
@@ -19,11 +19,15 @@ def iti_mu(date, window=4):
     date : Date
     window : int
         Number of seconds before the visual stim to include in calcualtion.
+    nan_artifacts : bool
+        Remove regions where dff values are crazy
+    thresh : int
+        Threshold in dff space for artifact removal (e.g. 5 = 500% dff)
 
     Result
     ------
     np.ndarray
-        An array of length equal to the number cells, values are mean
+        An array of length equal to the number cells. Values are mean
         cellular response during the pre-visual stim window.
 
     """
@@ -45,11 +49,21 @@ def iti_mu(date, window=4):
         mask = np.isin(np.convolve(mask, win, mode='same'), 1)
 
         masks.append(mask)
-        traces.append(t2p.d['dff'])
+        traces.append(t2p.trace('dff'))
 
     # concatenate across days
     traces = np.concatenate(traces, axis=1)
     masks = np.concatenate(masks, axis=0)
+
+    # remove huge artifacts from trace: blank with nans
+    if nan_artifacts:
+        noise = np.zeros(np.shape(traces))
+        noise[np.abs(traces) > thresh] = 1
+
+        # dialate blanking around threshold crossings
+        for cell in range(np.shape(traces)[0]):
+            noise[cell, :] = np.convolve(noise[cell, :], np.ones(3), mode='same')
+        traces[noise != 0] = np.nan
 
     # calculate masked mean per cell
     mu = np.nanmean(traces[:, masks], axis=1)
@@ -58,7 +72,7 @@ def iti_mu(date, window=4):
 
 
 @memoize(across='date', updated=190130, returns='cell array')
-def iti_sigma(date, window=4):
+def iti_sigma(date, window=4, nan_artifacts=False, thresh=8):
     """
     Calculate the standard deviation (sigma) of daily trace per
     cell avoiding visual stimulus period.
@@ -68,11 +82,15 @@ def iti_sigma(date, window=4):
     date : Date
     window : int
         Number of seconds before the visual stim to include in calcualtion.
+    nan_artifacts : bool
+        Remove regions where dff values are crazy
+    thresh : int
+        Threshold in dff space for artifact removal (e.g. 5 = 500% dff)
 
     Result
     ------
     np.ndarray
-        An array of length equal to the number cells, values are standard
+        An array of length equal to the number cells. Values are standard
         deviation of cellular response during the pre-visual-stim window.
 
     """
@@ -94,13 +112,201 @@ def iti_sigma(date, window=4):
         mask = np.isin(np.convolve(mask, win, mode='same'), 1)
 
         masks.append(mask)
-        traces.append(t2p.d['dff'])
+        traces.append(t2p.trace('dff'))
 
     # concatenate across days
     traces = np.concatenate(traces, axis=1)
     masks = np.concatenate(masks, axis=0)
 
+    # remove huge artifacts from trace: blank with nans
+    if nan_artifacts:
+        noise = np.zeros(np.shape(traces))
+        noise[np.abs(traces) > thresh] = 1
+
+        # dialate blanking around threshold crossings
+        for cell in range(np.shape(traces)[0]):
+            noise[cell, :] = np.convolve(noise[cell, :], np.ones(3), mode='same')
+        traces[noise != 0] = np.nan
+
     # calculate masked standard deviation per cell
     sigma = np.nanstd(traces[:, masks], axis=1)
+
+    return sigma
+
+
+@memoize(across='date', updated=190130, returns='cell array')
+def mu(date, nan_artifacts=False, thresh=8):
+    """
+    Calculate the mean (mu) of daily trace per cell.
+
+    Parameters
+    ----------
+    date : Date
+    nan_artifacts : bool
+        Remove regions where dff values are crazy
+    thresh : int
+        Threshold in dff space for artifact removal (e.g. 5 = 500% dff)
+
+    Result
+    ------
+    np.ndarray
+        An array of length equal to the number cells, values are mean
+        cellular response.
+
+    """
+
+    # get traces for whole day and a mask vector for pre-stim period
+    runs = date.runs()
+    traces = []
+    for run in runs:
+        t2p = run.trace2p()
+        traces.append(t2p.trace('dff'))
+
+    # concatenate across days
+    traces = np.concatenate(traces, axis=1)
+
+    # remove huge artifacts from trace: blank with nans
+    if nan_artifacts:
+        noise = np.zeros(np.shape(traces))
+        noise[np.abs(traces) > thresh] = 1
+
+        # dialate blanking around threshold crossings
+        for cell in range(np.shape(traces)[0]):
+            noise[cell, :] = np.convolve(noise[cell, :], np.ones(3), mode='same')
+        traces[noise != 0] = np.nan
+
+    # calculate masked mean per cell
+    mu = np.nanmean(traces, axis=1)
+
+    return mu
+
+
+@memoize(across='date', updated=190130, returns='cell array')
+def sigma(date, nan_artifacts=False, thresh=8):
+    """
+    Calculate the standard deviation (sigma) of daily trace per
+    cell.
+
+    Parameters
+    ----------
+    date : Date
+    nan_artifacts : bool
+        Remove regions where dff values are crazy
+    thresh : int
+        Threshold in dff space for artifact removal (e.g. 5 = 500% dff)
+
+    Result
+    ------
+    np.ndarray
+        An array of length equal to the number cells. Values are standard
+        deviation of cellular response.
+
+    """
+
+    # get traces for whole day and a mask vector for pre-stim period
+    runs = date.runs()
+    traces = []
+    for run in runs:
+        t2p = run.trace2p()
+        traces.append(t2p.trace('dff'))
+
+    # concatenate across days
+    traces = np.concatenate(traces, axis=1)
+
+    # remove artifacts from trace: blank with nans
+    if nan_artifacts:
+        noise = np.zeros(np.shape(traces))
+        noise[np.abs(traces) > thresh] = 1
+
+        # dialate blanking around threshold crossings
+        for cell in range(np.shape(traces)[0]):
+            noise[cell, :] = np.convolve(noise[cell, :], np.ones(3), mode='same')
+        traces[noise != 0] = np.nan
+
+    # calculate masked standard deviation per cell
+    sigma = np.nanstd(traces, axis=1)
+
+    return sigma
+
+
+@memoize(across='run', updated=190130, returns='cell array')
+def run_mu(run, nan_artifacts=False, thresh=8):
+    """
+    Calculate the mean (mu) of single run trace per cell.
+
+    Parameters
+    ----------
+    run : Run
+    nan_artifacts : bool
+        Remove regions where dff values are crazy
+    thresh : int
+        Threshold in dff space for artifact removal (e.g. 5 = 500% dff)
+
+    Result
+    ------
+    np.ndarray
+        An array of length equal to the number cells. Values are mean
+        cellular response.
+
+    """
+
+    # get traces for whole day and a mask vector for pre-stim period
+    t2p = run.trace2p()
+    traces = t2p.trace('dff')
+
+    # remove huge artifacts from trace: blank with nans
+    if nan_artifacts:
+        noise = np.zeros(np.shape(traces))
+        noise[np.abs(traces) > thresh] = 1
+
+        # dialate blanking around threshold crossings
+        for cell in range(np.shape(traces)[0]):
+            noise[cell, :] = np.convolve(noise[cell, :], np.ones(3), mode='same')
+        traces[noise != 0] = np.nan
+
+    # calculate masked mean per cell
+    mu = np.nanmean(traces, axis=1)
+
+    return mu
+
+
+@memoize(across='run', updated=190130, returns='cell array')
+def run_sigma(run, nan_artifacts=False, thresh=8):
+    """
+    Calculate the standard deviation (sigma) of single run trace per
+    cell.
+
+    Parameters
+    ----------
+    date : Date
+    nan_artifacts : bool
+        Remove regions where dff values are crazy
+    thresh : int
+        Threshold in dff space for artifact removal (e.g. 5 = 500% dff)
+
+    Result
+    ------
+    np.ndarray
+        An array of length equal to the number cells. Values are standard
+        deviation of cellular response.
+
+    """
+
+    # get traces for single run and a mask vector for pre-stim period
+    t2p = run.trace2p()
+    traces = t2p.trace('dff')
+
+    # remove huge artifacts from trace: blank with nans
+    if nan_artifacts:
+        noise = np.zeros(np.shape(traces))
+        noise[np.abs(traces) > thresh] = 1
+
+        # dialate blanking around threshold crossings
+        for cell in range(np.shape(traces)[0]):
+            noise[cell, :] = np.convolve(noise[cell, :], np.ones(3), mode='same')
+        traces[noise != 0] = np.nan
+
+    # calculate masked standard deviation per cell
+    sigma = np.nanstd(traces, axis=1)
 
     return sigma
