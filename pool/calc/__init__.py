67ab06fd959efae5d5d5c30f2e270ac371017a8b
Jeff Zaremba <jzaremba@gmail.com>
2019-02-12 17:06:29 -0500

    Fix for * import

--- pool/calc/__init__.py
+++ pool/calc/__init__.py
@@ -5,4 +5,7 @@ All calc functions must be imported on initialization to allow the memoizer
 to build a dictionary of update dates.
 
 """
+import os
+import glob
+__all__ = [os.path.basename(f)[:-3] for f in glob.glob(os.path.dirname(__file__)+"/*.py")]
 from . import *
