03fab5f1824b296b38572166d5d4b1b99d15ad58
Arthur Sugden <arthur.sugden@gmail.com>
2019-02-26 11:32:09 -0500

    Added many new flags including drop_glm_zeros. Have not completely extended the changes to the 'run' version yet.

--- pool/calc/distance.py
+++ pool/calc/distance.py
@@ -2,9 +2,9 @@
 import numpy as np
 from scipy.spatial.distance import cosine
 try:
-    from bottleneck import nanmean, nanmedian
+    from bottleneck import nanmean, nanmedian, nanmax
 except ImportError:
-    from numpy import nanmean, nanmedian
+    from numpy import nanmean, nanmedian, nanmax
 
 from ..database import memoize
 from .. import stimulus
@@ -59,16 +59,121 @@ def cosdist_continuous(
     return np.array(result)
 
 
-@memoize(across='date', updated=190220, returns='value')
+@memoize(across='date', updated=190225, returns='value')
 def cosine_similarity_stimuli(
-        date, cs, group, trace_type='deconvolved', start_s=0, end_s=1, trange_glm=(0, 1), rectify=True,
-        exclude_outliers=True, remove_group=None):
+        date, cs, group, trace_type='deconvolved', start_s=0, end_s=1, trange_glm=(0, 1), rectify=False,
+        exclude_outliers=False, remove_group=None, offset_glm_positive=False, remove_baseline_stimuli=False,
+        drop_glm_zeros=False, max_across_trial=False, compare_to_trials=False, error_trials=0, binarize=None,
+        correlation=False):
     """
     Calculate the cosine distance between a GLM protovector and population.
 
     Parameters
     ----------
     date : Date
+    cs : str
+        The stimulus type to compare to.
+    group : str
+        Group in GLM.groups, e.g. 'ensure'.
+    trace_type : str
+        Type of trace to compare to the GLM protovector.
+    start_s : int
+        Start time of stimulus
+    end_s : int
+        End time of stimulus
+    trange_glm : 2-element tuple of float
+        Time range to look at for the protovector.
+    rectify : bool
+        If True, set negative values of the protovector to 0.
+    exclude_outliers : bool
+        If True, exclude cells with the highest activity.
+    remove_group : str, optional
+        If not None, remove this group protovector from the result.
+    offset_glm_positive : bool
+        If True, subtract the minimum from the GLM protovector to force all values positive.
+    remove_baseline_stimuli : bool
+        Not yet implemented. If true, remove offsets, running, and brain motion from stimuli.
+    drop_glm_zeros : bool
+        If True, remove all cells in which the GLM protovector is <= 0.
+    max_across_trial : bool
+        If True, take the max across trials rather than the mean.
+    compare_to_trials : bool
+        If True, compare to individual trials rather than the mean trial.
+    error_trials : int {-1, 0, 1}
+        If -1, all trials, 0, correct trials, 1, incorrect trials.
+    binarize : bool
+        Not implemented yet. If true, binarize both vectors.
+    correlation : bool
+        If true, use correlation rather than cosine distance.
+
+    Returns
+    -------
+    np.ndarray
+        Array of length number of time points corresponding to the cosine
+        distance between the specified protovector and the population response.
+
+    """
+
+    if remove_baseline_stimuli:
+        raise NotImplementedError('Have not yet made it possible to remove brainmotion, running, and offsets from stimuli.')
+
+    if binarize:
+        raise NotImplementedError('Have not yet identified values for GLM and stim binarization.')
+
+    stimuli = stimulus.trials(date, cs, start_s=start_s, end_s=end_s,
+                              trace_type=trace_type, error_trials=error_trials)
+    if max_across_trial:
+        stimuli = nanmax(stimuli, axis=1)
+    else:
+        stimuli = nanmean(stimuli, axis=1)
+
+    if exclude_outliers:
+        keep = cell_activity.keep(date)
+        stimuli = stimuli[keep]
+    else:
+        keep = np.zeros(len(stimuli)) < 1
+
+    glm = date.glm()
+    unit = glm.protovector(
+        group, trange=trange_glm, rectify=rectify, err=-1,
+        remove_group=remove_group)[keep]
+
+    if offset_glm_positive and np.min(unit) < 0:
+        unit -= np.min(unit)
+
+    if drop_glm_zeros:
+        keep = unit > 0
+        unit = unit[keep]
+        stimuli = stimuli[keep]
+
+    if compare_to_trials:
+        out = []
+        if correlation:
+            for s in range(np.shape(stimuli)[1]):
+                out.append(np.corrcoef(stimuli[:, s], unit)[0, 1])
+        else:
+            for s in range(np.shape(stimuli)[1]):
+                out.append(1.0 - cosine(stimuli[:, s], unit))
+        return nanmean(out)
+    else:
+        stimuli = nanmean(stimuli, axis=1)
+        if correlation:
+            return np.corrcoef(stimuli, unit)[0, 1]
+        else:
+            return 1.0 - cosine(stimuli, unit)
+
+
+# @memoize(across='run', updated=190220, returns='value')
+def cosine_similarity_stimuli_run(
+        run, cs, group, trace_type='deconvolved', start_s=0, end_s=1, trange_glm=(0, 1), rectify=True,
+        exclude_outliers=True, remove_group=None, offset_glm_positive=False, remove_baseline_stimuli=False,
+        drop_zeros=False, max_across=False, compare_to_trials=False):
+    """
+    Calculate the cosine distance between a GLM protovector and population.
+
+    Parameters
+    ----------
+    run : Date
     group : str
         Group in GLM.groups.
     tracetype : str
@@ -91,19 +196,30 @@ def cosine_similarity_stimuli(
 
     """
 
-    stimuli = stimulus.trials(date, cs, start_s=start_s, end_s=end_s, trace_type=trace_type)
+    stimuli = stimulus.trials(run, cs, start_s=start_s, end_s=end_s, trace_type=trace_type, error_trials=0)
+
+
+
     stimuli = nanmean(stimuli, axis=2)
     stimuli = nanmean(stimuli, axis=1)
 
     if exclude_outliers:
-        keep = cell_activity.keep(date)
+        keep = cell_activity.keep(run)
         stimuli = stimuli[keep]
     else:
         keep = np.zeros(len(stimuli)) < 1
 
-    glm = date.glm()
+    glm = run.parent.glm()
     unit = glm.protovector(
         group, trange=trange_glm, rectify=rectify, err=-1,
         remove_group=remove_group)[keep]
+
+    if offset_glm_positive and np.min(unit) < 0:
+        unit -= np.min(unit)
+
+    if drop_zeros:
+        keep = unit > 0
+        unit = unit[keep]
+        stimuli = stimuli[keep]
 
     return 1.0 - cosine(stimuli, unit)
