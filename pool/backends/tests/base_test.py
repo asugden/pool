e50cc098b4ac7018d4c97315bce3af87af62254b
Jeff Zaremba <jzaremba@gmail.com>
2019-03-08 21:42:23 -0500

    Added additional tests to the backend tests.

--- pool/backends/tests/base_test.py
+++ pool/backends/tests/base_test.py
@@ -2,12 +2,12 @@ from builtins import object
 from six import with_metaclass
 
 from abc import ABCMeta, abstractmethod
+from copy import deepcopy
 from numpy.testing import run_module_suite, assert_array_equal, assert_equal
 import numpy as np
 import pandas as pd
+import uuid
 
-host = 'localhost'
-database = 'testing'
 
 class BaseTests(with_metaclass(ABCMeta, object)):
 
@@ -142,6 +142,42 @@ class BaseTests(with_metaclass(ABCMeta, object)):
             self.db.recall(analysis_name=analysis_name, keys=self.keys)
         assert_array_equal(val2.index, val.index)
         assert_array_equal(val2.values, val.values)
+
+    def test_dependencies(self):
+        analysis_name = 'test_depends'
+        val = 2.
+        depends_on = {'test_float': self.updated}
+        self.db.store(
+            analysis_name=analysis_name, data=val, keys=self.keys,
+            updated=self.updated, depends_on=depends_on)
+        val2, updated2, dependcies2 = \
+            self.db.recall(analysis_name=analysis_name, keys=self.keys)
+        assert_equal(val2, val)
+        assert_equal(dependcies2, depends_on)
+
+    def test_missing_key(self):
+        analysis_name = 'test_missing_key'
+        keys = deepcopy(self.keys)
+        keys['mouse'] = str(uuid.uuid1())
+        val2, updated2, dependencies2 = \
+            self.db.recall(analysis_name=analysis_name, keys=keys)
+        assert_equal(val2, None)
+        assert_equal(updated2, None)
+        assert_equal(dependencies2, None)
+
+    def test_parameters(self):
+        analysis_name = 'test_parameters'
+        keys = deepcopy(self.keys)
+        keys['foo'] = 'bar'
+        keys['dogs'] = 7
+        val = 42
+        self.db.store(
+            analysis_name=analysis_name, data=val, keys=keys,
+            updated=self.updated)
+        val2, updated2, dependcies2 = \
+            self.db.recall(analysis_name=analysis_name, keys=keys)
+        assert_equal(val2, val)
+
 
 if __name__ == "__main__":
     run_module_suite()
