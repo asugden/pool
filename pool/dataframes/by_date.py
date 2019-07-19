74f7ef6ff9ab94fa86e9a908984ba41b4813f626
Arthur Sugden <arthur.sugden@gmail.com>
2019-07-19 13:29:09 -0400

    Reverted to previous version to include jupyter function for Jeff.

--- pool/dataframes/by_date.py
+++ pool/dataframes/by_date.py
@@ -1,3 +1,6 @@
+import datetime
+import numpy as np
+
 import pandas as pd
 
 import flow.grapher
@@ -8,6 +11,150 @@ import flow.sorters
 from flow.misc.type_conversion import nannone
 from .. import calc
 from .. import calc_legacy
+from .. import database
+
+
+def dataframe_date(date):
+    """
+
+    Parameters
+    ----------
+    date
+
+    Returns
+    -------
+
+    """
+
+    df = pd.DataFrame([date.mouse], columns=['mouse'])
+    # df['mouse'] = date.mouse
+    df['date'] = date.date
+    df['date_obj'] = df['date'].apply(
+        lambda x: datetime.datetime.strptime(str(x), '%y%m%d'))
+    df['dprime'] = calc_legacy.performance.dprime(date)
+    df['dprime_new'] = calc.performance.dprime(date)
+    df['dprime_new_alltrials'] = calc.performance.dprime(
+        date, hmm_engaged=False)
+    df['dprime_run'] = calc.performance.dprime(date, across_run=False)
+    df['dprime_day_start'] = calc.psytrack.dprime_day_start(date)
+    df['dprime_day_end'] = calc.psytrack.dprime_day_end(date)
+    df['d_dprime_day'] = calc.psytrack.d_dprime_day(date)
+    df['reversed'] = flow.metadata.reversal(date.mouse) < date.date
+
+    # df['react_plus'] = nannone(calc_legacy.reactivation_rate.freq(date, 'plus'))
+    # df['react_neutral'] = nannone(calc_legacy.reactivation_rate.freq(date, 'neutral'))
+    # df['react_minus'] = nannone(calc_legacy.reactivation_rate.freq(date, 'minus'))
+
+    df['react_new_plus'] = nannone(calc.reactivation_rate.freq(date, 'plus'))
+    df['react_new_neutral'] = nannone(calc.reactivation_rate.freq(date, 'neutral'))
+    df['react_new_minus'] = nannone(calc.reactivation_rate.freq(date, 'minus'))
+    df['react_new_comb'] = \
+        df['react_new_plus'] + df['react_new_neutral'] + df['react_new_minus']
+    df['react_all_plus'] = nannone(calc.reactivation_rate.freq(date, 'plus', state='all'))
+    df['react_all_neutral'] = nannone(calc.reactivation_rate.freq(date, 'neutral', state='all'))
+    df['react_all_minus'] = nannone(calc.reactivation_rate.freq(date, 'minus', state='all'))
+
+    # df['cosdist_plus'] = nannone(calc_legacy.cosine_distance.stimulus(date, 'plus'))
+    # df['cosdist_neutral'] = nannone(calc_legacy.cosine_distance.stimulus(date, 'neutral'))
+    # df['cosdist_minus'] = nannone(calc_legacy.cosine_distance.stimulus(date, 'minus'))
+
+    # cosine_similarity_stimuli(
+    #     date, cs, group, trace_type='deconvolved', start_s=0, end_s=1, trange_glm=(0, 1), rectify=False,
+    #     exclude_outliers=False, remove_group=None, offset_glm_positive=False, remove_baseline_stimuli=False,
+    #     drop_glm_zeros=False, max_across_trial=False, compare_to_trials=False, error_trials=0, binarize=None,
+    #     correlation=False)
+
+
+
+    return df
+
+
+def date_fraction(df):
+    """
+    Add the fractional time passed for each mouse.
+
+    Parameters
+    ----------
+    df : Dataframe
+
+    Returns
+    -------
+    updated dataframe
+
+    """
+
+    for mouse in df['mouse'].unique():
+        df.loc[df.mouse == mouse, 'sequential_date'], _ = \
+            pd.factorize(df.loc[df.mouse == mouse, 'date'], sort=True)
+        df.loc[df.mouse == mouse, 'fractional_date'] = \
+            (df.loc[df.mouse == mouse, 'sequential_date'].astype(float)/
+             df.loc[df.reversed == 0, 'sequential_date'].max())
+
+    return df
+
+
+def main(args):
+    """Main function."""
+    sorter = flow.sorters.DateSorter.frommeta(
+        mice=args.mice, dates=args.dates, tags=args.tags)
+    # np.warnings.filterwarnings('ignore')
+
+    df = None
+    for date in sorter:
+        df_date = dataframe_date(date)
+
+        if df is None:
+            df = df_date
+        else:
+            df = pd.concat([df, df_date], ignore_index=True, sort=True)
+
+    df = date_fraction(df)
+
+    return df
+
+
+def parse_args():
+    arg_parser = flow.misc.default_parser(
+        description="""
+        Plot clusters across pairs of days.""",
+        arguments=('mice', 'tags', 'dates'))
+    arg_parser.add_argument(
+        '-p', '--save_path', type=str, default=flow.paths.graphcrossday(),
+        help='The directory in which to save the output graph.')
+
+    args = arg_parser.parse_args()
+
+    return args
+
+
+def jupyter(mice=None, dates=None, tags=None):
+    """
+    Pass the dataframe to a jupyter notebook.
+
+    Parameters
+    ----------
+    mice : list of strings
+        Mouse names
+    dates : list of ints
+        Dates
+    tags : list of strings
+        Tags
+
+    Returns
+    -------
+    dataframe across days
+
+    """
+
+    class TempArgs:
+        def __init__(self):
+            self.mice = mice
+            self.dates = dates
+            self.tags = tags
+
+    # args = parse_args()
+    args = TempArgs()
+    return main(args)
 
 
 def example_date_function(date, df):
